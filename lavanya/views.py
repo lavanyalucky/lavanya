5293bbd2294c9b457431ca3a47780126aa952cdb
lavanyalucky <lavanyalucky164@gmail.com>
2023-03-07 19:32:35 +0530

    first commit

---
+++ lavanya/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def  bahubali(request):
+    return HttpResponse('nuvvu naa pakkana unnanta varaku nannu chanpey magadu puttaledu mamaaa.....')
